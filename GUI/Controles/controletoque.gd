a58920103f203e53c77ecc1b7f79affcf6dcfca2
LucianoFO11 <lucianosaulo05@gmail.com>
2024-09-24 16:42:38 -0300

    "Adição de nível 1 e 2 para debug e mudança no movimento"

--- GUI/Controles/controletoque.gd
+++ GUI/Controles/controletoque.gd
@@ -2,7 +2,7 @@ extends CanvasLayer
 @onready var esquerda = $Esquerda
 @onready var direita = $Direita
 @onready var pulo = $Pulo
-
+@onready var descer = $Descer
 
 
 func _on_esquerda_pressed():
@@ -29,3 +29,11 @@ func _on_pulo_pressed():
 
 func _on_pulo_released():
 	pulo.modulate.a = 1.0
+
+
+func _on_descer_pressed():
+	descer.modulate.a = 1.0
+
+
+func _on_descer_released():
+	descer.modulate.a = 0.5
